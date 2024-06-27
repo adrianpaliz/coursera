db5247a7e731db6d08f9d61e20048f05e0f74222
adrianpaliz <adrianpaliz@gmail.com>
2024-06-27 00:06:18 -0500

    Lesson about variables

---
+++ meta/python_101/lessons/variables.py
@@ -0,0 +1,46 @@
+# variable name, variable assignment, actual value
+#my_variable = 0 
+
+#Naming convention
+#Camel case
+#myName = 0
+#Snake case
+#my_name = 0
+'''
+my_variable_two = 6
+print(my_variable_two)
+'''
+#You can declare any diferent type of variable in terms of value
+#For example a string
+'''
+my_string_variable = 'Hello'
+print(my_string_variable)
+'''
+#You can declare multiple variables and assing them to a single value
+'''
+a = b = c = 10
+print(a)
+print(b)
+print(c)
+'''
+#Multiple assingments 
+'''
+a, b, c = 1, 2, 3
+print(a)
+print(b)
+print(c)
+'''
+#A variable is subject to change
+'''
+a = 10
+print(a)
+a = 5
+print(a)
+'''
+#Deleting a variable
+a = 11
+print(a)
+del a
+print(a)
+
+
