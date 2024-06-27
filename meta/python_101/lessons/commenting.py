32c5eb7b79d725c9539f2c2b181f81e5c9abd9fe
adrianpaliz <adrianpaliz@gmail.com>
2024-06-26 23:35:04 -0500

    Lessons about commenting code

---
+++ meta/python_101/lessons/commenting.py
@@ -0,0 +1,13 @@
+#One line
+#Don't try to Rum Me, I'm a comment
+my_variable = 10
+
+#Multi-line comments (Phython does not really have a method of declaring multiline comments)
+# The function below accepts two integers and
+# adds both numbers to return the value.
+def add_two_numbers(number_one, number_two):
+    return number_one + number_two
+
+#In line comments
+my_variable = 1 #Declaring a variable
+
