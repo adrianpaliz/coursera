f93a9b41fabd1dc520e8a00fe98b8573d9da71c5
adrianpaliz <adrianpaliz@gmail.com>
2024-06-27 00:42:02 -0500

    Lesson about data types

---
+++ meta/python_101/lessons/basic_data_types.py
@@ -0,0 +1,53 @@
+#Python data types
+'''
+1 Numeric
+2 Sequence
+3 Dictionary
+4 Boolean
+5 Set
+'''
+
+#Type function()
+#print(type('hello'))
+
+#Numeric data types
+'''
+Integers: 10 or -10
+Floats: 10.5 or 6.7
+Complex numbers: a = 10 +10j
+'''
+
+#Sequence data types
+'''
+Strings: 
+name = 'Jim'
+Lists:
+example_list = [1, 'hello', 4.5, 'A']
+Tuples:
+example_tuple = (2, 'world', 3.2, 'B')
+'''
+
+#Dictionary data types
+#ed = {'a' : 22, 'b' : 44.4}
+
+#Boolean data types
+#print(type(False))
+
+#Set data types
+'''
+example_set = {4, 'test', 1.2, 'C'}
+print(type(example_set))
+'''
+
+a = 10
+print(type(a))
+
+b = 2.3
+print(type(b))
+
+c = 'text'
+print(type(c))
+
+d = [1, 2, 3, 4]
+print(type(d))
+
