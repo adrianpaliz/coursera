52c474ec4eaf7783cf79c8259b8d74e1e8ecd939
adrianpaliz <adrianpaliz@gmail.com>
2024-07-10 07:07:40 -0500

    Lesson about strings

---
+++ meta/python_101/lessons/strings.py
@@ -0,0 +1,23 @@
+# Declarin a string
+variable_1 = "Hello word"
+# Declarin a multiple line string
+variable_2 = 'This is a multi' \
+        ' line string example'
+print(variable_1)
+print(variable_2)
+
+# You can access to individual characters using index
+name = 'Adrián'
+print(name[0])
+print(name[1])
+print(name[2])
+
+# Using len() function: return the number of characters
+print(len(name))
+
+# String concatenation
+string_1 = 'Hello '
+string_2 = 'there'
+print(string_1 + string_2)
+
+
