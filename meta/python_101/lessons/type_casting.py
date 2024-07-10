bc86b0a16996a9955fb4da391c0d2ee13f99a732
adrianpaliz <adrianpaliz@gmail.com>
2024-07-10 07:29:44 -0500

    Lesson about type casting

---
+++ meta/python_101/lessons/type_casting.py
@@ -0,0 +1,12 @@
+# Converting the specified value into a string
+integer = 11
+print(str(integer))
+
+# Converting into a integer
+float_value = 20.5
+print(int(float_value))
+
+# Converting into a float
+integer = 5
+print(float(integer))
+
