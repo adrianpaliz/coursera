cdb462f2acfa4ed3b2ca51685c0672115fdf1add
adrianpaliz <adrianpaliz@gmail.com>
2024-07-11 16:49:19 -0500

    Lesson about user input and console output

---
+++ meta/python_101/lessons/user_input_console_output.py
@@ -0,0 +1,52 @@
+# User input
+'''
+email = input('Please enter your email address: ')
+print(email)
+'''
+# Print arguments
+# Comma separated
+#print(1, 2, 3,)
+
+# Aritmetic
+#print(1 + 3)
+
+# String concatenation
+'''
+name = 'Adrián'
+print('Imanalla ' + name)
+'''
+
+# Print function has reserved keywords that can be arguments
+#print('Hello', 'you', sep=', ')
+
+# Direct formating
+'''
+value_1 = 10
+value_2 = 5
+sum_values = value_1 + value_2
+print('Adding the value of {} and {} = {}'. format(value_1, value_2, sum_values ))
+'''
+# Output formating
+'''
+print('I like {0} more than {1}'. format('oranges', 'grapes'))
+print('I like {1} more than {0}'. format('oranges', 'grapes'))
+'''
+# Python input and output
+'''
+number = input('Please enter a number: ')
+print(number)
+'''
+number_1 = input('Please enter the first number: ')
+number_2 = input('Please enter a second number: ')
+print(number_1, number_2)
+
+# We can do arithmetic operations
+print(number_1 + number_2) # Adding strings
+print(int(number_1) + int(number_2)) # Adding integers
+
+# Checking the data type
+print(type(number_1))
+
+
+
+
