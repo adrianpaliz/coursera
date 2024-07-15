d1fce98834696ac535ce990e85b71d10868f3935
adrianpaliz <adrianpaliz@gmail.com>
2024-07-15 16:38:46 -0500

    Lesson about math and logical operators

---
+++ meta/python_101/lessons/math_and_logical_operators.py
@@ -0,0 +1,31 @@
+# Operations can be: matematical, logical and comparison
+'''
+Matematical
+Plus sign + for addition
+Minus sign - to subtraction
+Forward slash / for division
+Asterisk * for multiplication
+'''
+'''
+print(2 + 2)
+print(2 - 2)
+print(35 / 5)
+print(35 * 5)
+'''
+'''
+Logical
+'and' Checks for both conditions to be true
+'or' Checks for at least one conditions to be true
+'not' Returns false if the result is true
+'''
+a = False
+b = False
+if a and b:
+    print('All true!')
+
+if a or b:
+    print('All true!')
+
+if not(a) or not(b):
+    print('All true')
+
