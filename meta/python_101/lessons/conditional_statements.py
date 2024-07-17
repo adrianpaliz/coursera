bc500c7116a04d840a1864db272b0e249fe181e5
adrianpaliz <adrianpaliz@gmail.com>
2024-07-16 20:40:57 -0500

    Example code about conditional statements

---
+++ meta/python_101/lessons/conditional_statements.py
@@ -0,0 +1,11 @@
+# Light is currently off
+current = False
+
+if current:
+    current = False
+    print('Turning light off')
+
+if not current:
+    current = True
+    print('Turning light on')
+
