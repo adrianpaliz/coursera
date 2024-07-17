d6a0cf27ff257fced8bd112d7efb8b00966f93db
adrianpaliz <adrianpaliz@gmail.com>
2024-07-16 21:08:07 -0500

    Another example about conditional statements

--- meta/python_101/lessons/conditional_statements.py
+++ meta/python_101/lessons/conditional_statements.py
@@ -1,11 +1,15 @@
-# Light is currently off
-current = False
+loyalty_customer = True
+total_bill = 124
 
-if current:
-    current = False
-    print('Turning light off')
-
+if loyalty_customer and total_bill > 100:
+    # Give 20% discount
+    total_bill = total_bill - (float(total_bill) / 100) * 20
+elif total_bill > 100:
+    # Give 10% discount
+    total_bill = total_bill - (float(total_bill) / 100) * 10
 else:
-    current = True
-    print('Turning light on')
+    # Sorry no discount, 5% service charge applied.
+    print('Sorry, no discount...')
+
+print('Total Bill: ', float(total_bill))
 
