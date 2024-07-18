e65d4642eb25761cd63205c09cb6d155a8682f18
adrianpaliz <adrianpaliz@gmail.com>
2024-07-18 16:27:20 -0500

    Lesson about looping constructions

---
+++ meta/python_101/lessons/looping_constructions.py
@@ -0,0 +1,17 @@
+#for index in range(5):
+#    print('Looping...', index)
+
+favorites = ['Cuy', 'Tigrillo', 'Bandera', 'Bolón', 'Chicha']
+
+#for item in favorites:
+#    print('I like this meal', item)
+for index, item in enumerate(favorites):
+    print(index, item)
+
+'''
+count = 0
+while count < len(favorites):
+    print('I like this meal', favorites[count])
+    count += 1
+'''
+
