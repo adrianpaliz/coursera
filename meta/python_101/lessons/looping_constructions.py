4e877bf94c22d03eebee89bc064f4d630c8681d4
adrianpaliz <adrianpaliz@gmail.com>
2024-07-18 23:26:28 -0500

    Lesson about Practical control flow and loops

--- meta/python_101/lessons/looping_constructions.py
+++ meta/python_101/lessons/looping_constructions.py
@@ -5,8 +5,8 @@ favorites = ['Cuy', 'Tigrillo', 'Bandera', 'Bolón', 'Chicha']
 
 #for item in favorites:
 #    print('I like this meal', item)
-for index, item in enumerate(favorites):
-    print(index, item)
+#for index, item in enumerate(favorites):
+#    print(index, item)
 
 '''
 count = 0
@@ -14,4 +14,18 @@ while count < len(favorites):
     print('I like this meal', favorites[count])
     count += 1
 '''
+'''
+for item in favorites:
+    if item == 'Bandera':
+        print('Yes! one of my favorite meals is', item)
+        continue
+    else:
+        print('No sorry, that meal is not on my list')
+'''
+
+for item in favorites:
+    if item == 'Bandera':
+        pass
+    print('Other meal I like is', item)
+
 
