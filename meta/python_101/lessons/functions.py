1a1643ac1c9487c2df0fffdf59f4afb2c37b861b
adrianpaliz <adrianpaliz@gmail.com>
2024-07-20 10:18:19 -0500

    Lesson about functions, we rewrite the previous example code as a function

--- meta/python_101/lessons/functions.py
+++ meta/python_101/lessons/functions.py
@@ -1,6 +1,8 @@
-bill = 175.00
-tax_rate = 15
+def calculate_tax(bill, tax_rate):
+    return round((bill * tax_rate / 100.00), 2)
 
-total_tax = (bill * tax_rate / 100.00)
-print('Total tax', total_tax)
+print('Total tax:', calculate_tax(175.00, 15))
+
+print('Total tax:', calculate_tax(164.33, 22))
+
 
