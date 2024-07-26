01f0898dd989bebf90586d1b5b3510397029399a
adrianpaliz <adrianpaliz@gmail.com>
2024-07-26 18:58:03 -0500

    Basic example code about exception handling

---
+++ meta/python_101/lessons/exception_handling.py
@@ -0,0 +1,17 @@
+# Explore two types of error
+# Syntax error: which are caused by human errors (misspelling or typo)
+# A commom error for new developers learning in Python is not adding the colon at the end of conditions or statements
+# Other commom mistake is indentation problems
+
+# Exceptions: which are known errors that need to be handled
+
+# Example
+def divide_by(a, b):
+    return a / b
+
+try:
+    ans = divide_by(40, 0)
+except:
+    print("Something went wrong!")
+
+
