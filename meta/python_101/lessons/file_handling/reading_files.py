7533a4b63d08a3144e6421a685aa7032796fbedd
adrianpaliz <adrianpaliz@gmail.com>
2024-07-28 17:48:32 -0500

    We have change the for loop to point to the file variable who returns a list by default

--- meta/python_101/lessons/file_handling/reading_files.py
+++ meta/python_101/lessons/file_handling/reading_files.py
@@ -2,8 +2,10 @@
 with open('python_101/lessons/file_handling/sample_file.txt', 'r') as file:
     #print(file.readlines())
     # Because it's a list, I can assing it to a varaiable.
-    data = file.readlines()
-    for line in data:
+    #data = file.readlines()
+    # When you use with open and you get as file it returns a list by default
+    # We can change the for loop so that it points to the file variable
+    for line in file:
         print(line)
 
     
