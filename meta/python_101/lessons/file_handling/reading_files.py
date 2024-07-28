1007f51416252bb11584a2109aaf0948be1e584e
adrianpaliz <adrianpaliz@gmail.com>
2024-07-28 17:40:45 -0500

    Looping through the list that readlines function give us

--- meta/python_101/lessons/file_handling/reading_files.py
+++ meta/python_101/lessons/file_handling/reading_files.py
@@ -1,6 +1,11 @@
 # Reading files
 with open('python_101/lessons/file_handling/sample_file.txt', 'r') as file:
-    print(file.readlines())
+    #print(file.readlines())
+    # Because it's a list, I can assing it to a varaiable.
+    data = file.readlines()
+    for line in data:
+        print(line)
+
     
 
 
