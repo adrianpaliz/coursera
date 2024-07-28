d234022df4b71242b1d76cd485c63e6b7f44a373
adrianpaliz <adrianpaliz@gmail.com>
2024-07-28 16:52:37 -0500

    Dealing with exceptions by using the try and accept statements

--- meta/python_101/lessons/file_handling/creating_files.py
+++ meta/python_101/lessons/file_handling/creating_files.py
@@ -1,5 +1,8 @@
 # Creating files
-# We use 'w' for remplace or 'a' to append to the current file
-with open('python_101/lessons/file_handling/new_file.txt', 'a') as file:
-    file.writelines(['\nThis is a new file created!', '\nThis is another line to be added'])
-
+try:
+    # We use 'w' for remplace or 'a' to append to the current file
+    with open('sample/python_101/lessons/file_handling/new_file.txt', 'a') as file:
+        file.writelines(['\nThis is a new file created!', '\nThis is another line to be added'])
+except FileNotFoundError as error:
+    print('Error!', error)
+    
