da9aea572ebfd45ec20b7ccab8cdf138c1ddf87c
adrianpaliz <adrianpaliz@gmail.com>
2024-07-28 16:46:48 -0500

    Creating files with multiple text lines

--- meta/python_101/lessons/file_handling/creating_files.py
+++ meta/python_101/lessons/file_handling/creating_files.py
@@ -1,4 +1,5 @@
 # Creating files
-with open('python_101/lessons/file_handling/new_file.txt', 'w') as file:
-    file.write("This is a new file created!")
+# We use 'w' for remplace or 'a' to append to the current file
+with open('python_101/lessons/file_handling/new_file.txt', 'a') as file:
+    file.writelines(['\nThis is a new file created!', '\nThis is another line to be added'])
 
