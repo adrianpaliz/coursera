57535aaf86814986a22e71381cdc3263271289dc
adrianpaliz <adrianpaliz@gmail.com>
2024-07-30 12:48:29 -0500

    We improve our code using binary search, it takes only 7 iterations to find the number this time

--- meta/python_101/lessons/algorithmic_complexity.py
+++ meta/python_101/lessons/algorithmic_complexity.py
@@ -1,10 +1,21 @@
-def find_number(number_to_search):
-    count = 0
-    for number in range(100):
-        if number == number_to_search:
-            print('Total iterations: ', count)
-            return number
-        count += 1
+def find_number_log(target):
+    iterations = 0
+    number = range(100)
+    left = 0
+    right = len(number) - 1
+    
+    while left <= right:
+        iterations += 1
+        middle = (left + right) // 2
+        possible_number = number[middle]
 
-find_number(97)
+        if target == possible_number:
+            print('Iterations', iterations)
+            return middle
+        elif target < possible_number:
+            right = middle - 1
+        else:
+            left = middle + 1
+
+print(find_number_log(97))
 
