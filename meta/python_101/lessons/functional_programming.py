546a166544e099110a87a2d2d2cb34cca45d1108
adrianpaliz <adrianpaliz@gmail.com>
2024-07-31 22:35:14 -0500

    Lesson about functional programming

---
+++ meta/python_101/lessons/functional_programming.py
@@ -0,0 +1,6 @@
+# Functional programing in essence is a programming paradigm that utilizes functions for clean, consistent and maintainable code
+# Functional programming:
+    # Does not change the data outside the scope of the function.
+    # Is easy to maintain and saves a lot of development time.
+    # Utilizes reusable functions for clean and elegant code.
+
