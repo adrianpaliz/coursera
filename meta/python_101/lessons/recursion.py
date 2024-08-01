661fff09466bc8cd91c9934ea1954989512d6171
adrianpaliz <adrianpaliz@gmail.com>
2024-07-31 22:58:10 -0500

    Lesson about recursion

---
+++ meta/python_101/lessons/recursion.py
@@ -0,0 +1,14 @@
+# Recursion is used for solving problems that can be broken down into smaller repetitive problems.
+# It's especially good for working on things that have many possible branches and too complex for an iterative approach.
+# Recursion is essentially a function that calss itself.
+
+# Advantages:
+    # Neat code and less bulky
+    # Complex tasks can be broke down into easier to read sub-problems
+    # Generation of sequences can be easier to understand that nested loops
+
+# Disadvantages:
+    # Hard to follow the logic
+    # In terms of memory, they are expensive and sometimes inefficient.
+    # Difficult to debug
+
