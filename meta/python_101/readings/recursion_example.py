9eed768b977e566ef288e7165bfe751da22aed03
adrianpaliz <adrianpaliz@gmail.com>
2024-08-02 23:55:15 -0500

    Recursion example: Tower of Hanoi

---
+++ meta/python_101/readings/recursion_example.py
@@ -0,0 +1,18 @@
+# Recursive function for Tower of Hanoi
+def tower_of_hanoi(disks, source, helper, destination):
+    # Base condition
+    if disks == 1:
+        print('Disk {} moves from tower {} to tower {}.'.format(disks, source, destination))
+        return
+
+    # Recursive calls in which function calls itself
+    tower_of_hanoi(disks - 1, source, destination, helper)
+    print('Disk {} moves from tower {} to tower {}.'.format(disks, source, destination))
+    tower_of_hanoi(disks - 1, helper, source, destination)
+
+# Driver code
+disks = int(input('Number of disks to be displaced: '))
+
+# Actual function call
+tower_of_hanoi(disks, 'source', 'helper', 'destination')
+        
