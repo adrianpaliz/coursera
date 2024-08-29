6d165d481e7527ed30d5f0baf38a7127a99d2fac
adrianpaliz <adrianpaliz@gmail.com>
2024-08-28 23:21:15 -0500

    Reading and exercise about: Define a class

---
+++ meta/python_101/readings/define_a_class.py
@@ -0,0 +1,30 @@
+# Class definition
+class Apartment:
+    # Data members or attributes
+    number_rooms = 3
+    bathrooms = 1
+    # Function definition(empty)
+    def cost_evaluation(self):
+        print(self.number_rooms)
+        # The pass keyword
+        pass
+# Instantiating tha class
+myapartment = Apartment()
+
+# A print statement refering to the instance object
+print(myapartment.number_rooms)
+# A print statement refreing to the class object
+print(Apartment.number_rooms)
+
+# Update the number_rooms variable
+# it updates the value of the instance attribute but not the class attribute 
+myapartment.number_rooms = 7
+print(myapartment.number_rooms)
+# The number_rooms attribute of the class remains unchanged as 3
+print(Apartment.number_rooms)
+
+# Modifying the class attribute 
+Apartment.number_rooms = 7
+print(myapartment.number_rooms)
+print(Apartment.number_rooms)
+
