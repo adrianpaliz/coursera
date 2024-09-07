bcd22bed65083f99c86de1805c55ddb731295323
adrianpaliz <adrianpaliz@gmail.com>
2024-09-07 16:47:32 -0500

    Lesson about: modules, packeges and libraries

---
+++ meta/python_101/readings/module_use_cases.py
@@ -0,0 +1,15 @@
+# Modules
+# Are similar to files
+# Generally are written in a single file
+# Can contain clases, functions and data members just like any other Python file.
+
+# Packages
+# Are like directories that contain different files
+# Are a type of module
+# Any module that contains the __path__ definition is a package
+# Can contain sub-packages and other modules.
+
+# Library
+# Is a term that's used interchangeably with importes packages.
+# It refers to a collection of packages.
+
